7eab046a975bae0006956668b388fd53c6935e65
cattle <413042326@qq.com>
2017-10-17 10:51:59 +0800

    "新增一个可以修改质量分数的工具，为做测试用的"

---
+++ src/main/java/org/broadinstitute/hellbender/tools/spark/modifiedSamRecord/SetBaseQuality.java
@@ -0,0 +1,49 @@
+package org.broadinstitute.hellbender.tools.spark.modifiedSamRecord;
+
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.broadinstitute.barclay.argparser.Argument;
+import org.broadinstitute.barclay.argparser.CommandLineProgramProperties;
+import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
+import org.broadinstitute.hellbender.cmdline.programgroups.SparkProgramGroup;
+import org.broadinstitute.hellbender.engine.spark.GATKSparkTool;
+import org.broadinstitute.hellbender.utils.read.GATKRead;
+import org.broadinstitute.hellbender.utils.read.markduplicates.ReadsKey;
+import scala.Tuple2;
+
+@CommandLineProgramProperties(
+        summary ="setBaseQualities on Spark",
+        oneLineSummary ="setBaseQualities on Spark",
+        programGroup = SparkProgramGroup.class)
+public final class SetBaseQuality extends GATKSparkTool {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public boolean requiresReads() { return true; }
+
+    @Argument(doc = "the output bam", shortName = StandardArgumentDefinitions.OUTPUT_SHORT_NAME,
+            fullName = StandardArgumentDefinitions.OUTPUT_LONG_NAME, optional = false)
+    protected String output;
+    
+    public static GATKRead setNewBaseQualities(GATKRead read,byte quality){
+        byte[] originalQual = read.getBaseQualities();
+        for(int i = 0;i<originalQual.length;i++){
+            originalQual[i] = quality;
+        }
+        read.setBaseQualities(originalQual);
+        return read;
+        
+    }
+    @Override
+    protected void runTool(final JavaSparkContext ctx) {
+        JavaRDD<GATKRead> reads = getReads();
+        JavaRDD<GATKRead> firstReads = reads.filter(read->read.isFirstOfPair())
+                                            .map(firstRead->setNewBaseQualities(firstRead,(byte) 16));
+        JavaRDD<GATKRead> secondReads = reads.filter(read->read.isSecondOfPair())
+                                             .map(secondRead->setNewBaseQualities(secondRead,(byte)18));
+        JavaRDD<GATKRead> wholeReads = firstReads.union(secondReads);
+        
+        writeReads(ctx,output,wholeReads);
+    }
+}
